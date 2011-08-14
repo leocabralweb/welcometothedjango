1f21212cd03b7288e9e117d9f4720d8fc4feb2fe
unknown <leonardo.cabral@stormsec.com.br>
2011-08-14 16:55:12 -0300

    All you base are belong to us.

--- settings.py
+++ settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'eventex.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, 'templates'),
